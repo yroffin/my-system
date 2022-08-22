c566a79a5d0d2f1f233f6c1218b1f0b3c85b341c
yroffin <yroffin@gmail.com>
2022-08-22 18:00:18 +0200

    Delete strapi/dist directory

--- strapi/dist/src/api/node/routes/node.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * node router.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreRouter('api::node.node');
