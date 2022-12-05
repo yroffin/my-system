9a355c38bd026dac20b477ed83e6732ded050690
Yannick Roffin <yroffin@gmail.com>
2022-12-05 01:06:34 +0100

    Fix

--- front/src/app/services/preferences.service.ts
+++ front/src/app/services/preferences.service.ts
@@ -4,7 +4,7 @@ import * as _ from 'lodash';
 import { SysEdge, SysGraph, SysNode } from '../models/graph';
 import { DatabaseService } from './database.service';
 import { Parser } from 'xml2js';
-import { NGXLogger } from 'ngx-logger';
+import { NGXLogger, NgxLoggerLevel } from 'ngx-logger';
 import { Base16Service } from './base16.service';
 import { MessageService } from 'primeng/api';
 import { LocalStorageService } from 'ngx-webstorage';
@@ -14,7 +14,6 @@ const parser = new Parser();
 
 @Injectable({ providedIn: 'root' })
 export class PreferenceService extends DatabaseEntity<SysPreference> {
-
     constructor(
         private _logger: NGXLogger,
         private _storage: LocalStorageService
@@ -36,4 +35,22 @@ export class PreferenceService extends DatabaseEntity<SysPreference> {
         }
     }
 
+    apply() {
+        let preferences = this.findOne("default")
+        if (preferences?.debug) {
+            // Get the current config
+            var config = this._logger.getConfigSnapshot();
+            // Updating only one field
+            config.level = NgxLoggerLevel.DEBUG;
+            // Setting the config
+            this._logger.updateConfig(config);
+        } else {
+            // Get the current config
+            var config = this._logger.getConfigSnapshot();
+            // Updating only one field
+            config.level = NgxLoggerLevel.INFO;
+            // Setting the config
+            this._logger.updateConfig(config);
+        }
+    }
 }
